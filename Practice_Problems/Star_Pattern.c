c007358205c4e30d7d7f29454bc4a01f2e6c4095
Zephryon-zv <ddas38192@gmail.com>
2023-10-26 18:10:44 +0530

    Added Stars_Pattern.c

--- Practice_Problems/Star_Pattern.c
+++ Practice_Problems/Star_Pattern.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void StarPattern(int count_rows) {
+    char obj = '*';
+    for (int i = 0; i <= count_rows; i++) {
+        for (int j = 0; j <= count_rows; j++) {
+            printf("%c", obj);
+        }
+        printf(" \n");
+    }
+}
+
 int main() {
     // Star Pattern
+    int count_rows;
+    printf("Enter the number rows: ");
+    scanf("%d", &count_rows);
+
+    StarPattern(count_rows);
+
+    return 0;
 }
