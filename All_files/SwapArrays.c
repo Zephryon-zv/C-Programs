a622498f7d16b2df53430298753d1e9110743c64
Zephryon-zv <ddas38192@gmail.com>
2023-12-24 13:50:44 +0530

    Update

---
+++ All_files/SwapArrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void Swap_values(int *x, int *y){
+    // function for swaping the value by reference
+    int temp_x = *y;
+    *y = *x;
+    *x = temp_x;
+}
+
+int main(){
+    // Program to swap values
+    int a = 23;
+    int b = 44;
+
+    printf("A = %d, B = %d\n", a,b);
+    printf("Address of A = %p, B = %p\n", &a, &b);
+    Swap_values(&a, &b);
+    printf("A = %d, B = %d", a, b);
+    return 0;
+}
